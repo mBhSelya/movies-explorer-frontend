bb4a69ae53a487b0f32d335f1c87db458ce8bc41
Aleksey Selyakov <selyakoff18@gmail.com>
2022-05-25 01:29:42 +0300

    feat: Ревью 1. Доработка интерфейса

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -38,8 +38,8 @@ export default function Profile(props) {
         console.log(props.isValid);
         console.log(name === userData.name);
         console.log(email === userData.email);
-        console.log(name === userData.name & email === userData.email & !props.isValid)
-        if (name === userData.name & email === userData.name & !props.isValid) {
+        console.log(name === userData.name && email === userData.email && !props.isValid)
+        if (name === userData.name && email === userData.name && !props.isValid) {
             setChangedData(true);
         } else {
             setChangedData(false);
